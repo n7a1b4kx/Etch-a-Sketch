ac45a7a65e458607070dc0cae1b2c682e856a273
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-09 21:47:39 +0500

    Add: func for adding sketch-boxes dynamically also style
    now sketch-boxes can be added dynamically upon entring a value into input field
    also style the sketch boxes using js methods

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 
 const SKETCH_PAD = document.querySelector('.sketch-pad');
-
-for (let j = 0; j < 14; j++) {
-    for (let i = 0; i < 14; i++) {
-        const BOX = document.createElement('div');
-        BOX.classList.add('sketch-box');
-        SKETCH_PAD.appendChild(BOX);
-        BOX.addEventListener('mouseover', () => {
-            BOX.setAttribute('style', 'background-color: burlywood;');
-        });
-    }
-}
+const SKETCH_PAD_DIMENSIONS = window.getComputedStyle(SKETCH_PAD).maxWidth;
 
 function applyGridSize() {
+    SKETCH_PAD.innerHTML = '';
     const gridSizeInput = document.querySelector("#grid-size-input");
     const gridSize = gridSizeInput.value;
+    const BOX_DIMENSIONS = parseInt(SKETCH_PAD_DIMENSIONS) / gridSize;
+
+    for (let j = 0; j < gridSize; j++) {
+        for (let i = 0; i < gridSize; i++) {
+            const BOX = document.createElement('div');
+            BOX.classList.add('sketch-box');
+            BOX.setAttribute('style', `height: ${BOX_DIMENSIONS}px; width: ${BOX_DIMENSIONS}px;`);
+            SKETCH_PAD.appendChild(BOX);
+            BOX.addEventListener('mouseover', () => {
+                BOX.setAttribute('style', `background-color: burlywood; height: ${BOX_DIMENSIONS}px; width: ${BOX_DIMENSIONS}px;`);
+            });
+        }
+    }
+
+    console.log(BOX_DIMENSIONS);
     console.log(gridSize);
 }
