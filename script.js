57c97032974ae9674580068efe622e79fdab8800
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-09 22:02:07 +0500

    Add: default sketch-pad & sketch-boxes

--- script.js
+++ script.js
@@ -2,6 +2,26 @@
 const SKETCH_PAD = document.querySelector('.sketch-pad');
 const SKETCH_PAD_DIMENSIONS = window.getComputedStyle(SKETCH_PAD).maxWidth;
 
+function onStart() {
+    const gridSize = 32;
+    const BOX_DIMENSIONS = parseInt(SKETCH_PAD_DIMENSIONS) / gridSize;
+
+    for (let j = 0; j < gridSize; j++) {
+        for (let i = 0; i < gridSize; i++) {
+            const BOX = document.createElement('div');
+            BOX.classList.add('sketch-box');
+            BOX.setAttribute('style', `height: ${BOX_DIMENSIONS}px; width: ${BOX_DIMENSIONS}px;`);
+            SKETCH_PAD.appendChild(BOX);
+            BOX.addEventListener('mouseover', () => {
+                BOX.setAttribute('style', `background-color: burlywood; height: ${BOX_DIMENSIONS}px; width: ${BOX_DIMENSIONS}px;`);
+            });
+        }
+    }
+
+}
+
+onStart();
+
 function applyGridSize() {
     SKETCH_PAD.innerHTML = '';
     const gridSizeInput = document.querySelector("#grid-size-input");
@@ -19,7 +39,4 @@ function applyGridSize() {
             });
         }
     }
-
-    console.log(BOX_DIMENSIONS);
-    console.log(gridSize);
 }
