97a377fa7d4f0d8a9febd5f3d4e9d41d02eb1e9a
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-12-12 08:39:20 +0500

    fix: create a func for creation of boxes --repetition

--- script.js
+++ script.js
@@ -1,47 +1,23 @@
 
 const SKETCH_PAD = document.querySelector('.sketch-pad');
 const SKETCH_PAD_DIMENSIONS = window.getComputedStyle(SKETCH_PAD).maxWidth;
+const gridSizeInput = document.querySelector("#grid-size-input");
 
 let backgroundColor = '#111720'
 
 function onStart() {
     const gridSize = 32;
-    const BOX_DIMENSIONS = parseInt(SKETCH_PAD_DIMENSIONS) / gridSize;
-
-    for (let j = 0; j < gridSize; j++) {
-        for (let i = 0; i < gridSize; i++) {
-            const BOX = document.createElement('div');
-            BOX.classList.add('sketch-box');
-            BOX.setAttribute('style', `height: ${BOX_DIMENSIONS}px; width: ${BOX_DIMENSIONS}px;`);
-            SKETCH_PAD.appendChild(BOX);
-            BOX.addEventListener('mouseover', () => {
-                BOX.setAttribute('style', `background-color: ${backgroundColor}; height: ${BOX_DIMENSIONS}px; width: ${BOX_DIMENSIONS}px;`);
-            });
-        }
-    }
-
+    populateSketchPad(gridSize);
 }
 
 onStart();
 
 function applyGridSize() {
     SKETCH_PAD.innerHTML = '';
-    const gridSizeInput = document.querySelector("#grid-size-input");
+    
     const gridSize = (gridSizeInput.value === '' || gridSizeInput.value <= 0) ? 32: gridSizeInput.value;
     (gridSizeInput.value === '' || gridSizeInput.value <= 0) ? alert('Please first enter Grid Size!'): null;
-    const BOX_DIMENSIONS = parseInt(SKETCH_PAD_DIMENSIONS) / gridSize;
-
-    for (let j = 0; j < gridSize; j++) {
-        for (let i = 0; i < gridSize; i++) {
-            const BOX = document.createElement('div');
-            BOX.classList.add('sketch-box');
-            BOX.setAttribute('style', `height: ${BOX_DIMENSIONS}px; width: ${BOX_DIMENSIONS}px;`);
-            SKETCH_PAD.appendChild(BOX);
-            BOX.addEventListener('mouseover', () => {
-                BOX.setAttribute('style', `background-color: ${backgroundColor}; height: ${BOX_DIMENSIONS}px; width: ${BOX_DIMENSIONS}px;`);
-            });
-        }
-    }
+    populateSketchPad(gridSize);
 }
 
 function clearSketchPad() {
@@ -49,4 +25,24 @@ function clearSketchPad() {
     BOXES.forEach(box => {
         box.style.backgroundColor = '#ffffff';
     });
+}
+
+function populateSketchPad(gridSize) {
+    const BOX_DIMENSIONS = parseInt(SKETCH_PAD_DIMENSIONS) / gridSize;
+
+    for (let j = 0; j < gridSize; j++) {
+        for (let i = 0; i < gridSize; i++) {
+            const BOX = document.createElement('div');
+            BOX.classList.add('sketch-box');
+            BOX.setAttribute('style', `height: ${BOX_DIMENSIONS}px; width: ${BOX_DIMENSIONS}px;`);
+            SKETCH_PAD.appendChild(BOX);
+            BOX.addEventListener('mouseover', () => {
+                BOX.setAttribute('style', `background-color: ${backgroundColor}; height: ${BOX_DIMENSIONS}px; width: ${BOX_DIMENSIONS}px;`);
+            });
+        }
+    }
+}
+
+function inputValidation(gridSize) {
+    (gridSize )
 }
